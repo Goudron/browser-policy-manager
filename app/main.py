8474bf454a5512523393877b3f547e2d8182faeb
Valery Ledovskoy <valery@ledovskoy.com>
2025-10-05 12:11:58 +0300

    chore: initial scaffold

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import os
+
+app = FastAPI(title="Browser Policy Manager (MVP)")
+templates = Jinja2Templates(directory=os.path.join(os.path.dirname(__file__), "templates"))
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "message": "It works!"})
